197295179fabc9ef58bd00c9fe15fc10815181c4
Jade Tran <jade@liveip.org>
2019-03-16 17:27:50 -0700

    improved logging

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -55,7 +55,9 @@ public class UrlValidatorTest extends TestCase {
       String[] invalidUrls = new String[testRounds];
       int invalidUrlsCount = 0;
       System.out.println("\n***START isValid TEST***\n");
-      System.out.println("\nTesting combinations of valid URL parts to form valid URLs:");
+      System.out.println("Testing combinations of valid URL parts to form valid URLs");
+
+      UrlValidator validator = new UrlValidator();
 
       for(int i = 0; i < testRounds; i++) {
          // randomize url parts by getting random index for each array
@@ -66,7 +68,6 @@ public class UrlValidatorTest extends TestCase {
          int queriesIndex = (int) (Math.random() *2);
 
          String url = validSchemes[schemeIndex] + validAuthority[authorityIndex] + validPort[portIndex] + validPath[pathIndex] + validQueries[queriesIndex];
-         UrlValidator validator = new UrlValidator();
 
          // use the isValid function to check if the final Url is valid
          boolean valid = validator.isValid(url);
@@ -81,13 +82,25 @@ public class UrlValidatorTest extends TestCase {
       }
 
       // print results
-      System.out.println("\nNumber of invalid Urls from valid URL parts: " + bugCount);
-      System.out.println("\nInvalid URLs: \n");
-      for (int j = 0; j < invalidUrls.length; j++) {
-         if(invalidUrls[j] != null){
-            System.out.println(invalidUrls[j] + "\n");
+      if(bugCount >0){
+         System.out.println("\nNumber of invalid Urls from valid URL parts: " + bugCount);
+         System.out.println("\nInvalid URLs: \n");
+         for (int j = 0; j < invalidUrls.length; j++) {
+            if(invalidUrls[j] != null){
+               System.out.println(invalidUrls[j] + "\n");
+            }
          }
+
       }
+
+      try{
+         assertEquals(0, bugCount);
+         System.out.println("Result: Passed");
+      }catch(Error e){
+         System.out.println("Result: Failed\n" + e);
+         throw e;
+      };
+
       System.out.println("\n***END isValid TEST***\n");
 
 
@@ -97,28 +110,41 @@ public class UrlValidatorTest extends TestCase {
    public void testisValidAuthority()
    {
       UrlValidator validator = new UrlValidator();
-      System.out.println("\n***START isValidAuthority TEST***\n");
-      System.out.println("\nTesting www.google.com:80");
-      assertEquals( true,validator.isValidAuthority("www.google.com:80"));
-      System.out.println("\nTesting www.google.com:3000");
-      assertEquals(true,validator.isValidAuthority("www.google.com:3000"));
-      System.out.println("\nTesting www.google.com:900000");
-      assertEquals(false,validator.isValidAuthority("www.google.com:900000"));
-      System.out.println("\n***END isValidAuthority TEST***\n");
+      System.out.println("\n***START isValidAuthority TEST***");
+      try{
+         System.out.println("\nTesting www.google.com:80");
+         assertEquals( true,validator.isValidAuthority("www.google.com:80"));
+         System.out.println("\nTesting www.google.com:3000");
+         assertEquals(true,validator.isValidAuthority("www.google.com:3000"));
+         System.out.println("\nTesting www.google.com:900000");
+         assertEquals(false,validator.isValidAuthority("www.google.com:900000"));
+         System.out.println("Result: Passed");
+      }catch(Error e){
+         System.out.println("Result: Failed\n" + e);
+         throw e;
+      };
+
+      System.out.println("\n***END isValidAuthority TEST***");
    }
 
    public void testisValidSchemeTest()
    {
       UrlValidator validator = new UrlValidator();
       System.out.println("\n***START isValidScheme TEST***\n");
-      System.out.println("\nTesting http");
-      assertEquals( true,validator.isValidScheme("http"));
-      System.out.println("\nTesting https");
-      assertEquals( true,validator.isValidScheme("https"));
-      System.out.println("\nTesting ftp");
-      assertEquals( true,validator.isValidScheme("ftp"));
-      System.out.println("\nTesting err");
-      assertEquals( false,validator.isValidScheme("err"));
+      try{
+         System.out.println("\nTesting http");
+         assertEquals( true,validator.isValidScheme("http"));
+         System.out.println("\nTesting https");
+         assertEquals( true,validator.isValidScheme("https"));
+         System.out.println("\nTesting ftp");
+         assertEquals( true,validator.isValidScheme("ftp"));
+         System.out.println("\nTesting err");
+         assertEquals( false,validator.isValidScheme("err"));
+         System.out.println("Result: Passed");
+      }catch(Error e){
+         System.out.println("Result: Failed\n" + e);
+         throw e;
+      };
       System.out.println("\n***END isValidScheme TEST***\n");
 
    }
